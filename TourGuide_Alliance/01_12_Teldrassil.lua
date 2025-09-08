01b9069ed21bbb3cea143375e9fff3c8416cdd9e
Trus3683 <152137641+Trus3683@users.noreply.github.com>
2025-09-08 00:32:57 -0700

    Update 01_12_Teldrassil.lua

--- TourGuide_Alliance/01_12_Teldrassil.lua
+++ TourGuide_Alliance/01_12_Teldrassil.lua
@@ -190,7 +190,7 @@ A Great Bear Spirit |QID|5929| |N|Dendrite Starblaze in Nighthaven (56.24, 30.64
 N Great Bear Spirit |QID|5929.1| |N|Speak to the Great Bear Spirit in Moonglade (39.09, 27.54)| |C|Druid| |Z|Moonglade|
 T Great Bear Spirit |QID|5929| |N|Dendrite Starblaze in Nighthaven (56.24, 30.64)| |C|Druid| |Z|Moonglade|
 A Back to Darnassus |QID|5931| |N|Dendrite Starblaze in Nighthaven (56.20, 30.64)| |C|Druid| |Z|Moonglade|
-F Rut'theran Village |TID|5931| |N|Speak to Silva Fil'naveth and fly to Rut'theran Village (58.33, 93.89)| |C|Druid| |Z|Moonglade|
+F Rut'theran Village |TID|5931| |N|Speak to Silva Fil'naveth and fly to Rut'theran Village (44.4, 45.4)| |C|Druid| |Z|Moonglade|
 T Back to Darnassus |QID|5931| |N|Mathrengyl Bearwalker in Cenarion Enclave (34.90, 8.25)| |C|Druid| |Z|Darnassus|
 A Body and Heart |QID|6001| |N|Mathrengyl Bearwalker in Cenarion Enclave (34.90, 8.25)| |C|Druid| |Z|Darnassus|
 
